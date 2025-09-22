ad8204229381b1f94efad9c4c8fe36fed5aee176
Juan Canchila <felipe.canchila1994@gmail.com>
2025-09-21 20:42:35 -0500

    Agregamos lista de amigos y stub para comprobar

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+//Inicio: Creando un array para agregar los nombres. Estado de la aplicación: 
+const amigos = [] //Esto nos permitirá guardar los nombres de los amigos en un tipo lista, colocamos const porque no vamos a reasignar la variable amigos
+
+// Stubs temporales para que el HTML no rompa
+function agregarAmigo() { 
+  console.log('agregarAmigo() llamado'); 
+}
+
+function sortearAmigo() { 
+  console.log('sortearAmigo() llamado'); 
+}
